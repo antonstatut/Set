d0fa8580f0a59d79ec86528527564748c7962ee5
Anton Statut <antonstatut@gmail.com>
2025-11-10 21:41:52 +0300

    initial commit

---
+++ src/js/__tests__/test.js
@@ -0,0 +1,27 @@
+import Team from '../index';
+
+test('добавление уникального персонажа в команду', () => {
+    const petr = { name: 'petr'}
+    const fedya = { name: 'fedya'}
+    Team.add(petr);
+    Team.add(fedya);
+    expect(Team.size).toBe(2);
+});
+
+test('добавление нескольких персонажей в команду', () => {
+    const petr = { name: 'petr'}
+    const fedya = { name: 'fedya'}
+    const tolya = { name: 'tolya'}
+
+    Team.addAll(petr, fedya, tolya);
+    expect(Team.size).toBe(3);
+});
+
+test("конвертация Set в массив", () => {
+       const petr = { name: 'petr'}
+       const fedya = { name: 'fedya'}
+  
+      Team.addAll(petr, fedya);
+  
+      expect(Team.toArray()).toEqual([petr, fedya]);
+});
---
+++ src/js/app.js
@@ -0,0 +1,25 @@
+class Team {
+    constructor() {
+        this.members = new Set();
+    }
+
+    add(character) {
+        if(this.members.has(character)){
+            throw new Error('Персонаж уже есть в команде');
+        }else{
+        this.members.add(character);
+        }
+    }
+
+    addAll(...characters){
+        characters.forEach(character => {
+            if(!this.members.has(character)){
+                this.members.add(character);
+            }
+        });
+    }
+
+    toArray() {
+        return Array.from(this.members);
+    }
+}
